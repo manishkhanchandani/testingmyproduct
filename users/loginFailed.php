09750fa924e0c95efa0808b86675981a716e6c43
naveenkhanchandani@gmail.com <naveenkhanchandani@gmail.com@d1b348a7-f8fa-0151-0e9a-958b523d4da7>
2014-02-06 23:06:54 +0000

    3

---
+++ users/loginFailed.php
@@ -0,0 +1,11 @@
+		<header>
+			<h2>Login Failed!</h2>
+			<span><a href="/users/login">Login</a> | <a href="/users/forgot">Forgot Password</a> | <a href="/users/register">Register</a></span>
+		</header>
+		<div class="container">
+			<div class="row">
+				<div class="12u">
+					Email and Password does not match with our records. Please go back and try again.
+				</div>
+			</div>
+		</div>
